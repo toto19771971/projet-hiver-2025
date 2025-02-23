2f430190b84efff55f53786921a5782b02f04776
Abdel MOURAK <a_mourak@yahoo.fr>
2025-02-23 21:17:52 +0100

    Fix Flask port binding for Render

--- app.py
+++ app.py
@@ -160,6 +160,7 @@ def traitement_salaires():
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Utilise le port donné par Render, sinon 5000 par défaut
+    app.run(host='0.0.0.0', port=port, debug=True)
